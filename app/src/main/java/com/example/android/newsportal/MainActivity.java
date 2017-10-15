1ded9a1d1af4d2a21615748d4c579cf9cef63577
Kevin Hartarto <k.hartarto9@gmail.com>
2017-10-15 21:21:23 +0700

    membuat carousel untuk main page

--- app/src/main/java/com/example/android/newsportal/MainActivity.java
+++ app/src/main/java/com/example/android/newsportal/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.android.newsportal;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static ViewPager mPager;
+    private static int currentPage= 0;
+    private static final Integer[] images = {R.drawable.ship, R.drawable.soerabaja, R.drawable.windmill};
+    private ArrayList<Integer> arrImages = new ArrayList<Integer>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        for(int i = 0; i < images.length;i++)
+            arrImages.add(images[i]);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
     }
 }
---
+++ app/src/main/java/com/example/android/newsportal/SliderAdapter.java
@@ -0,0 +1,54 @@
+package com.example.android.newsportal;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by KEVIN on 10/15/2017.
+ */
+
+public class SliderAdapter extends PagerAdapter{
+
+    //private variables
+    private ArrayList<Integer> images;
+    private LayoutInflater inflater;
+    private Context context;
+
+    //constructor
+    public SliderAdapter(ArrayList<Integer> images, Context context) {
+        this.context = context;
+        this.images = images;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public int getCount() {
+        return images.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup view, int position) {
+        View myImageLayout = inflater.inflate(R.layout.slide, view, false);
+        ImageView myImage = (ImageView) myImageLayout
+                .findViewById(R.id.image);
+        myImage.setImageResource(images.get(position));
+        view.addView(myImageLayout, 0);
+        return myImageLayout;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view.equals(object);
+    }
+}
