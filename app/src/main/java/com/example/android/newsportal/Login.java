aa20c40703d542da778ba65e3cfd0a7c9ff2b24f
Yandhy Raka <the_raka98@yahoo.co.id>
2017-11-07 12:18:20 +0700

    Ariel punya

---
+++ app/src/main/java/com/example/android/newsportal/Login.java
@@ -0,0 +1,222 @@
+package com.example.android.newsportal;
+        import android.app.ProgressDialog;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.net.ConnectivityManager;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.Toast;
+
+        import com.android.volley.Request;
+        import com.android.volley.Response;
+        import com.android.volley.VolleyError;
+        import com.android.volley.toolbox.StringRequest;
+        import com.example.android.newsportal.AppController;
+
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
+        import java.util.HashMap;
+        import java.util.Map;
+
+
+public class Login extends AppCompatActivity {
+
+
+    EditText email, password;
+    Button login, regis;
+    String URL= "http://10.0.2.2/projectandroid/Login.php";
+    ProgressDialog pDialog;
+    Intent intent;
+
+    int success;
+    ConnectivityManager conMgr;
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    public final static String TAG_EMAIL = "email";
+    public final static String TAG_ID = "id";
+
+    String tag_json_obj = "json_obj_req";
+
+    SharedPreferences sharedpreferences;
+    Boolean session = false;
+    String id, em;
+    public static final String my_shared_preferences = "my_shared_preferences";
+    public static final String session_status = "session_status";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        conMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        {
+            if (conMgr.getActiveNetworkInfo() != null
+                    && conMgr.getActiveNetworkInfo().isAvailable()
+                    && conMgr.getActiveNetworkInfo().isConnected()) {
+            } else {
+                Toast.makeText(getApplicationContext(), "No Internet Connection",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.pass);
+        login = (Button) findViewById(R.id.login);
+        regis = (Button) findViewById(R.id.button_register);
+
+
+        sharedpreferences = getSharedPreferences(my_shared_preferences, Context.MODE_PRIVATE);
+        session = sharedpreferences.getBoolean(session_status, false);
+        id = sharedpreferences.getString(TAG_ID, null);
+        em = sharedpreferences.getString(TAG_EMAIL, null);
+
+        if (session) {
+            Intent intent = new Intent(Login.this, MainActivity.class); //yang .class diganti ke class yang mau ditampilkan setelah Login
+            intent.putExtra(TAG_ID, id);
+            intent.putExtra(TAG_EMAIL, em);
+            finish();
+            startActivity(intent);
+        }
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String e = email.getText().toString();
+                String pas = password.getText().toString();
+
+                // mengecek kolom yang kosong
+                if (e.trim().length() > 0 && pas.trim().length() > 0) {
+                    if (conMgr.getActiveNetworkInfo() != null
+                            && conMgr.getActiveNetworkInfo().isAvailable()
+                            && conMgr.getActiveNetworkInfo().isConnected()) {
+                        checkLogin(e, pas);
+                    } else {
+                        Toast.makeText(getApplicationContext() ,"No Internet Connection", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    // Prompt user to enter credentials
+                    Toast.makeText(getApplicationContext() ,"Kolom tidak boleh kosong", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Login.this, MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void checkLogin(final String email, final String password) {
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Logging in ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "Login Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    success = jObj.getInt(TAG_SUCCESS);
+
+                    // Check for error node in json
+                    if (success == 1) {
+                        String email = jObj.getString(TAG_EMAIL);
+                        String id = jObj.getString(TAG_ID);
+
+                        Log.e("Successfully Login!", jObj.toString());
+
+                        Toast.makeText(getApplicationContext(), jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                        // menyimpan Login ke session
+//                        SharedPreferences.Editor editor = sharedpreferences.edit();
+//                        editor.putBoolean(session_status, true);
+//                        editor.putString(TAG_ID, id);
+//                        editor.putString(TAG_EMAIL, email);
+//                        editor.commit();
+
+                        // Memanggil main activity
+                        Intent intent = new Intent(Login.this, MainActivity.class); //ganti yg .class ke kelas selanjutnya setelah Login
+                        intent.putExtra(TAG_ID, id);
+                        intent.putExtra(TAG_EMAIL, email);
+                        finish();
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    // JSON error
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+
+                hideDialog();
+
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting parameters to Login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_json_obj);
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+
+}
+
+
